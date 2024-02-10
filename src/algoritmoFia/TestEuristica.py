9c3aa67631799e1f8d87a9f921708f4c514823e8
Roksid2002 <roksid09@gmail.com>
2024-02-10 11:11:33 +0100

    correzzione test consistenza

--- src/algoritmoFia/TestEuristica.py
+++ src/algoritmoFia/TestEuristica.py
@@ -1,10 +1,11 @@
 import pandas as pd
 from src.algoritmoFia.AlgAStar import distanza_haversine
 
+# Legge i dati dal file CSV
 file_path = "percorso_ottimaleGate3.csv"
 df = pd.read_csv(file_path, header=None, names=['latitudine', 'longitudine'], skiprows=1)
 
-# Converte la colonna della latitudine in numeri
+# Converte le colonne della latitudine e longitudine in numeri
 df['latitudine'] = pd.to_numeric(df['latitudine'], errors='coerce')
 
 # Converte la colonna della longitudine in numeri
@@ -13,9 +14,8 @@ df['longitudine'] = pd.to_numeric(df['longitudine'], errors='coerce')
 # Crea la lista delle coordinate
 percorso_ottimale = [(row['latitudine'], row['longitudine']) for index, row in df.iterrows()]
 
-
+# Funzione per verificare l'ammissibilità dell'euristica
 def ammissibile(percorso_ottimale):
-
     # Calcolo dell'euristica (distanza di Haversine) dallo start all'obiettivo
     start = percorso_ottimale[0]
     goal = percorso_ottimale[-1]
@@ -26,6 +26,7 @@ def ammissibile(percorso_ottimale):
     for i in range(len(percorso_ottimale) - 1):
         total_cost += distanza_haversine(percorso_ottimale[i][0], percorso_ottimale[i][1], percorso_ottimale[i+1][0], percorso_ottimale[i+1][1])
 
+    # Verifica se l'euristica è ammissibile
     if heuristic <= total_cost:
         print("ammissibile!")
     else:
@@ -34,11 +35,12 @@ def ammissibile(percorso_ottimale):
     # L'euristica è ammissibile se non sovrastima il costo per raggiungere l'obiettivo
     return heuristic <= total_cost
 
+# Funzione per testare la consistenza dell'euristica
 def testConsistenza(percorso_ottimale):
     # Individuazione del nodo obiettivo
     goal = percorso_ottimale[-1]
 
-    # Itera tutti i nodi del percorso ottimale
+    # Iterazione su tutti i nodi del percorso ottimale
     for i in range(len(percorso_ottimale) - 1):
         # Individua il nodo corrente e il nodo successivo
         current_node = percorso_ottimale[i]
@@ -54,17 +56,15 @@ def testConsistenza(percorso_ottimale):
         # Se il costo di passo più l'euristica dal nodo successivo è maggiore dell'euristica dal nodo corrente, l'euristica non è consistente
         if h_score_current > g_score + h_score_next:
             print("non consistente!")
-            #return None
 
     # Se nessuna incosistenza è stata trovata, l'euristica è consistente
     print("consistente!")
     return True
 
+# Verifica dell'ammissibilità dell'euristica
 ammissibilita = ammissibile(percorso_ottimale)
 print(ammissibilita)
 
+# Test della consistenza dell'euristica
 consistenza = testConsistenza(percorso_ottimale)
 print(consistenza)
-
-
-
